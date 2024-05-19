997b2f6ec8add8d18368d138fbe228d5a35cfd63
Matthias Balon <matthias.balon@epitech.eu>
2024-05-19 21:19:01 +0200

    feat : end condition

--- scene/map.gd
+++ scene/map.gd
@@ -9,3 +9,10 @@ func _ready():
 func _process(delta):
 	if Globals.player_life == 0:
 		get_tree().quit()
+
+
+func _on_area_2d_body_entered(body):
+	if (body.name != "player" && body.name != "ennemy"):
+		$Area2D/AnimatedSprite2D.play("on")
+		await get_tree().create_timer(2.0).timeout
+		get_tree().quit()
