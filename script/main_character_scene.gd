9e72447074efef0faf14c4c5e84e3b166f705191
git <matthias.balon@epitech.eu>
2024-05-19 09:23:17 +0200

    fix : the player cannot shoot multiple projectile at onec no more

--- script/main_character_scene.gd
+++ script/main_character_scene.gd
@@ -9,7 +9,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_pressed("shoot"):
+	if Input.is_action_just_pressed("shoot"):
 		shoot();
 
 func shoot():
